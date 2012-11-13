9b190c575e18f9e6f71f06169c0acd345eb3e1da
Baz Work <baz@work>
2012-11-13 12:58:38 +0000

    Starter Calculator class

---
+++ Ex5/Calculator.java
@@ -0,0 +1,21 @@
+
+public class Calculator {
+
+	/**
+	 * @param args
+	 */
+
+	
+	public void launch() {
+		System.out.println("Power TIME!");
+	}
+	
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator abacus = new Calculator();
+		abacus.launch();
+				
+	}
+
+}
