72982918a7bf0113baff1be43fd15728ac309feb
Baz Work <baz@work>
2012-11-13 14:57:55 +0000

    first revision

--- Ex5/Calculator.java
+++ Ex5/Calculator.java
@@ -1,3 +1,4 @@
+import java.io.Console;
 
 public class Calculator {
 
@@ -5,12 +6,53 @@ public class Calculator {
 	 * @param args
 	 */
 
-	
+	/**
+	 * launching class
+	 */
 	public void launch() {
-		System.out.println("Power TIME!");
+		System.out.println ( "Calculator has started!" );
+		System.out.println ( "Please pick from one of the following:" );
+		System.out.println ( "1 For 'Adding'" );
+		System.out.println ( "2 For 'Subtracting'" );
+		System.out.println ( "3 For 'multiplication'" );
+		System.out.println ( "4 For 'division'" );
+		System.out.println ( "0 to close..." );
+		
+		boolean finished = false;
+		
+		do {
+			String str = System.console().readLine();
+			//System.out.println (str);
+		    Integer choice = Integer.parseInt(str);
+    	    switch (choice) {
+    	        case 1:
+    	        // add
+    	        break;
+    	        case 2:
+    	        // subtract
+    	        break;
+    	        case 3:
+    	        // multiply
+    	        break;
+    	        case 4:
+    	        // divide
+    	        break;
+    	        case 0:
+    	        //finish
+    	        finished = true;
+    	        break;
+    	        default:
+    	        System.out.println( "That was not a valid choice! Please try again." );
+    	        break;
+    	    } 				
+		} while (!finished);
+		
+		System.out.println ( "Calculator App closing down..." );	
 	}
 	
-		
+	/**
+	 * Main class for sets up new class 
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calculator abacus = new Calculator();
