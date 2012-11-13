21b472bff04dcc37c147a88db2fda70b013ba76c
Baz Work <baz@work>
2012-11-13 16:22:01 +0000

    added modulus, tidied up a bit.

--- Ex5/Calculator.java
+++ Ex5/Calculator.java
@@ -31,6 +31,7 @@ public class Calculator {
 		System.out.println("2 For 'Subtracting'");
 		System.out.println("3 For 'Multiplication'");
 		System.out.println("4 For 'Division'");
+		System.out.println("5 For 'Modulus'");
 		System.out.println("0 to close...");
 		
 		String str = System.console().readLine();
@@ -40,39 +41,47 @@ public class Calculator {
 	
 	public void add(int x, int y) {
 		// Adder
-		System.out.println ( "...                            ..." );
+		System.out.println("...                            ...");
 		System.out.println("The answer for " + x + " PLUS " + y + " is " + (x + y));;
-		System.out.println ( "...                            ..." );
+		System.out.println("...                            ...");
 	}
 	
 	public void subtract(int x, int y) {
 		// Subtractor
-		System.out.println ( "...                            ..." );
+		System.out.println("...                            ...");
 		System.out.println("The answer for " + x + " MINUS " + y + " is " + (x - y));;
-		System.out.println ( "...                            ..." );
+		System.out.println("...                            ...");
 	}
 	
 	public void multiply(int x, int y) {
 		// Multiplier
-		System.out.println ( "...                            ..." );
+		System.out.println("...                            ...");
 		System.out.println("The answer for " + x + " TIMES " + y + " is " + (x * y));;
-		System.out.println ( "...                            ..." );
+		System.out.println("...                            ...");
 	}
 	
 	public void divisor(int x, int y) {
 		// Division
-		System.out.println ( "...                            ..." );
-		System.out.println("The answer for " + x + " DIVIDED BY " + y + " is " + (x / y));;
-		System.out.println ( "...                            ..." );
+		double total = (double) x / (double) y;
+		System.out.println("...                            ...");
+		System.out.println("The answer for " + x + " DIVIDED BY " + y + " is " + total);
+		System.out.println("...                            ...");
+	}
+	
+	public void mod(int x, int y) {
+		// Modulus
+		System.out.println("...                            ...");
+		System.out.println("The answer for " + x + " MOD " + y + " is " + (x % y));
+		System.out.println("...                            ...");
 	}
 	
 	/**
 	 * launching class
 	 */
 	public void launch() {
-		System.out.println ( "...                            ..." );
-		System.out.println ( "... Calculator App is starting ..." );
-		System.out.println ( "...                            ..." );
+		System.out.println( "...                            ...");
+		System.out.println( "... Calculator App is starting ...");
+		System.out.println( "...                            ...");
 		boolean finished = false;
 		do {
 			Calculator picker = new Calculator();
@@ -101,17 +110,23 @@ public class Calculator {
      	    	    picker.setX();
      	    	    picker.setY();
     	        	picker.divisor(picker.getX(), picker.getY());
-    	           	break;   	        
+    	           	break;
+    	        case 5:
+    	        	// divide
+     	    	    picker.setX();
+     	    	    picker.setY();
+    	        	picker.mod(picker.getX(), picker.getY());
+    	           	break;  
     	        case 0:
     	        	// finish
     	        	finished = true;
     	        	break;
     	        default:
-    	        	System.out.println( "That was not a valid choice! Please try again." );
+    	        	System.out.println( "That was not a valid choice! Please try again.");
     	        	break;
     	    }
 		} while (!finished);
-		System.out.println ( "...Calculator App closing down." );	
+		System.out.println("...Calculator App closing down.");	
 	}
 	
 	/**
