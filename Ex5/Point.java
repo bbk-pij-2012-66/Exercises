b26989c48027512abd8a42d254ca22ac7bf90fae
Baz Work <baz@work>
2012-11-13 11:51:07 +0000

    split launching method

--- Ex5/Point.java
+++ Ex5/Point.java
@@ -12,14 +12,17 @@ public class Point {
 		this.y = remote.y;
 	}
 	// more methods here...
-
-
-	public static void main(String[] args) {
+	
+	public void launch() {
 		Point point = new Point(1,1);
-		
 		System.out.println ( "The point is now at " + point.x + "," + point.y );
-		Point remotePoint = new Point(10,20);
+		Point remotePoint = new Point(10,18);
 		point.moveTo(remotePoint);
 		System.out.println ( "The point is now at " + point.x + "," + point.y );
+	}
+
+	public static void main(String[] args) {
+		Point starter = new Point(0,0);
+		starter.launch();
 	}
 }
