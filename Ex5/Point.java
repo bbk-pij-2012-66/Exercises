d1e014b24eb96ec08171278aa70c23b95eba2b54
Baz Work <baz@work>
2012-11-13 11:40:49 +0000

    revised to work in eclipse

---
+++ Ex5/Point.java
@@ -0,0 +1,25 @@
+public class Point {
+	double x;
+	double y;
+	
+	Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	void moveTo(Point remote) {
+		this.x = remote.x;
+		this.y = remote.y;
+	}
+	// more methods here...
+
+
+	public static void main(String[] args) {
+		Point point = new Point(1,1);
+		
+		System.out.println ( "The point is now at " + point.x + "," + point.y );
+		Point remotePoint = new Point(10,20);
+		point.moveTo(remotePoint);
+		System.out.println ( "The point is now at " + point.x + "," + point.y );
+	}
+}
