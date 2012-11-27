60fd0708080e2e45ffdf46b1e1f2c467651446e4
Baz Work <baz@work>
2012-11-27 10:10:19 +0000

    nov 27th update

---
+++ Ex4/day4.3a.groovy
@@ -0,0 +1,12 @@
+println "Mortgage Calculator!!";
+println "Please enter the total amount borrowed:";
+String str = System.console().readLine();
+double cost = Double.parseDouble(str);
+
+println "Please enter the number of years to pay it back:";
+str = System.console().readLine();
+int years = Integer.parseInt(str);
+
+println "Please enter the interest rate:";
+str = System.console().readLine();
+double cost = Double.parseDouble(str);
