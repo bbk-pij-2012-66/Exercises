6928d0757af9a3f590d0fb412de43e03d7c3bbc7
Baz Uni <Baz@Uni>
2012-12-03 18:26:40 +0000

    Exercise 1 day 10

---
+++ Ex10/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+	private final String name;
+	
+	public Person(String name) {
+		this.name = name;
+	}
+
+	public String getName() { // missing opening brace
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name; // final so can't change name
+	}
+}
