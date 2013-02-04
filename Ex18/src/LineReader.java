fd1052f0f7a73b573ea59e81d13c82e4d9f94f86
Baz Uni <Baz@Uni>
2013-02-04 19:49:34 +0000

    extra bits

---
+++ Ex18/src/LineReader.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+
+LineWriter (String fileName){
+	try {
+		
+	}
+	PrintWriter printWriter;
+	LineWriter(String fileName) {}
+	void writeLine(String )
+}
+
+public class LineReader(String fileName) {
+	FileReader fileReader = NULL;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+String readLine(){
+	try {
+		return bufferedReader.readLine();
+	}
+	
+	catch(IOException e) {
+		e.printStackTrace();
+	}
+	
+	return null;
+}
+
+void close() {
+	try {
+		bufferedReader.close();
+	}
+	
+	catch(IOException e) {}
+	
+}
