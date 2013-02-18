47f466d0c886b4e3193306424549227008a510eb
Baz Work <baz@work>
2013-02-18 16:01:20 +0000

    Created classes to executes the code in it's main method.
    Changing AdultPerson and KidPerson to verify that it still compiles.

---
+++ Ex7/Ex7.java
@@ -0,0 +1,17 @@
+
+public class Ex7 {
+
+	public void launch() {
+		AdultPerson Jimbo = new AdultPerson();
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ex7 starter = new Ex7();
+		starter.launch();
+	}
+
+}
---
+++ Ex7/KidPerson.java
@@ -0,0 +1,32 @@
+public class KidPerson implements Person {
+	private int position;
+	private Brain brain;
+	
+	/**
+	 * Move a distance in a straight line, given in metres
+	 */
+	public void move(int distance) {
+		crawl(distance);
+	}
+	
+	/**
+	 * Say something
+	 */
+	public void say(String message) {
+		for (int i = 0; i < distance; i++) {
+			position++;
+			waitALittle();
+		}
+	}
+	
+	private String getUnderstoodWords(String text) {
+		String result = "";
+		String[] words = brain.divideIntoWords(text);
+		for (int i = 0; i < words.length; i++) {
+			if (brain.isKnown(words[i])) {
+				result = result + words[i]; // if not, ignore it	
+			}
+		}
+		return result;
+	}
+}
